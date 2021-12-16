04d338c1a053385322591be09468a2a9705cb90d
PietroDomi <pietro.dominietto@gmail.com>
2021-12-16 18:41:49 +0100

    trial and run

--- examples/tsne_run.jl
+++ examples/tsne_run.jl
@@ -1,9 +1,10 @@
 using TsneBH
 
-N = 200
+N = 500
 X_dim = 100
 Y_dim = 2
 
-X = randn(N,X_dim);
+# X = randn(N,X_dim);
+X = TsneBH.random_start(N,X_dim,normal=true,seed=true)
 
-Y = tsne(X,Y_dim,100;lr=1.,verbose=true)
+Y = tsne(X,Y_dim,1000;lr=0.1,verbose=true,exag_fact=1.5)
